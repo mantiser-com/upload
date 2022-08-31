5ced7fd78464e01021ce6729ec22666e5ffb15ff
Mattias Hemmingsson <matte.hemmingsson@gmail.com>
2022-08-31 20:29:22 +0200

    Updated prokect

--- code/meilisSearch.py
+++ code/meilisSearch.py
@@ -9,10 +9,11 @@ client = meilisearch.Client(os.getenv('MEILISEARCH'))
 index = client.index('mantiser_page')
 documents = [
       { 'userid': 1, 'postid': 1, 'h1': ['data', 'data'] }]
-
-ret = index.add_documents(documents)
-print(ret)
-
+try: 
+  ret = index.add_documents(documents)
+  print(ret)
+except:
+  print("docs alreaddy there")
 try: 
   filter = client.index('mantiser_page').update_filterable_attributes([
     'userid',
@@ -36,14 +37,14 @@ except:
 def addMeilsearch(json_data):
     # An index is where the documents are stored.
     print(json_data)
-    user_index= "mantiser_"+str(json_data["projectID"])  #+"_"+json_data["userid"]
-    
-    client.create_index(user_index, {'primaryKey': 'id'})
-
+    try:
+      user_index= "mantiser_"+str(json_data["projectID"])  #+"_"+json_data["userid"]
+      client.create_index(user_index, {'primaryKey': 'id'})
+    except:
+      print("We have index")
     index = client.index(user_index)
     # If the index 'movies' does not exist, Meilisearch creates it when you first add the documents.
     #json_data["_id"]=0
-    #print(json_data)
     
     index.add_documents([json_data]) # => { "uid": 0 }
     print("Data added to melissearch")
